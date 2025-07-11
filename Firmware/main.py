898cfc41e519e4d267f5457d8d44d8d4397a8a14
tsudabyo <vinaykatta2007@gmail.com>
2025-07-11 10:45:20 -0500

    firmware update for encoder

--- Firmware/main.py
+++ Firmware/main.py
@@ -3,6 +3,7 @@ from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
 from kmk.extensions.media_keys import MediaKeys
+from kmk.extensions.encoder import EncoderHandler
 
 keyboard = KMKKeyboard()
 
@@ -14,6 +15,21 @@ keyboard.matrix = KeysScanner(
     pins=PINS,
     value_when_pressed=False,
 )
+
+# encoder 
+encoder = EncoderHandler()
+encoder.pins = (
+    (board.D6, board.D7),  # left encoder
+)
+keyboard.extensions.append(encoder)
+
+def handle_encoder_0(index, direction):
+    if direction == 1:
+        keyboard.send(KC.VOLU)  # volume up
+    elif direction == -1:
+        keyboard.send(KC.VOLD)  # volume down
+
+encoder.handlers = [handle_encoder_0]
 
 keyboard.keymap = [
     [KC.AUDIO_MUTE, KC.AUDIO_VOL_DOWN, KC.AUDIO_VOL_UP, KC.MEDIA_NEXT_TRACK]
